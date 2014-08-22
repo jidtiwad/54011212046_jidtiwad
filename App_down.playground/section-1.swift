c9410b467eefdd47ce087d78c14668697d959089
55011221032dong <student@tests-iMac-5.local>
2014-08-22 14:22:12 +0700

    my new app
    new week

---
+++ App_down.playground/section-1.swift
@@ -0,0 +1,20 @@
+let tutorialTeam = 56
+let editoriateam = 23
+var totalTeam = tutorialTeam + editoriateam
+
+totalTeam += 1
+let priceIntInferred = -19
+let priceIntExplicit:Int = -19
+
+let priceInferred = -19.99
+let priceExplicit:Double = -19.99
+
+let priceFloatInferred = -19.99
+let priceFloatExplicit:Float = -19.99
+
+let onSaleInferred = true
+let onsaleExplicit:Bool = false
+
+let nameInferred = "whoopie Cushion"
+
+
